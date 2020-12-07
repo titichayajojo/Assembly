567a96689071663f953561be608b3f071b299804
titichayajojo <72693468+titichayajojo@users.noreply.github.com>
2020-12-07 22:11:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 (function () {
     const config = {
         apiKey: "AIzaSyCk3BJ_e2twPTuw-ChARKGlv13XqCotce0",
-            authDomain: "assembly-8d1be.firebaseapp.com",
-            databaseURL: "https://assembly-8d1be.firebaseio.com",
-            projectId: "assembly-8d1be",
-            storageBucket: "assembly-8d1be.appspot.com",
-            messagingSenderId: "377358000231",
-            appId: "1:377358000231:web:a7e87650f49dc40b9bf4c2",
-            measurementId: "G-J0T1DCV1CS"
+        authDomain: "assembly-8d1be.firebaseapp.com",
+        databaseURL: "https://assembly-8d1be.firebaseio.com",
+        projectId: "assembly-8d1be",
+        storageBucket: "assembly-8d1be.appspot.com",
+        messagingSenderId: "377358000231",
+        appId: "1:377358000231:web:a7e87650f49dc40b9bf4c2",
+        measurementId: "G-J0T1DCV1CS"
     };
     firebase.initializeApp(config);
 
@@ -19,11 +19,13 @@
     const dbRefList = dbRefObject.child('dist');
     const dbRefListTemp = dbRefObject.child('temp');
 
+    dbRefObject.on('value', snap =>{
+        preObject.innerText = JSON.stringify(snap.val());
+    });
+
+
 
     dbRefList.on('value', snap => {
-        const li = document.createElement('li');
-        li.innerText = "Distance = " + snap.val();
-        ulList.appendChild(li);
 
         var chartObj = new FusionCharts({
             type: 'cylinder',
@@ -40,7 +42,7 @@
                     "upperLimit": "3000",
                     "lowerLimitDisplay": "Empty",
                     "upperLimitDisplay": "Full",
-                    "numberSuffix": " ltrs",
+                    "numberSuffix": " cm^3",
                     "showValue": "1",
                     "chartBottomMargin": "45",
                     "showValue": "0",
@@ -48,36 +50,30 @@
                 },
                 "value": "110",
                 "annotations": {
-                    "origw": "400",
+                    "origw": "250",
                     "origh": "190",
                     "autoscale": "1",
                     "groups": [{
                         "id": "range",
                         "items": [{
-                            "id": "rangeBg",
-                            "type": "rectangle",
-                            "x": "$canvasCenterX-45",
-                            "y": "$chartEndY-30",
-                            "tox": "$canvasCenterX +45",
-                            "toy": "$chartEndY-75",
-                            "fillcolor": "#6caa03"
+                            
                         }, {
                             "id": "rangeText",
                             "type": "Text",
                             "fontSize": "11",
                             "fillcolor": "#333333",
-                            "text": "0 ltrs",
+                            "text": "0 cm^3",
                             "x": "$chartCenterX-45",
                             "y": "$chartEndY-50"
                         }]
                     }]
                 }
-        
+
             },
             "events": {
-                "rendered": function(evtObj, argObj) {
+                "rendered": function (evtObj, argObj) {
                     var fuelVolume = 110;
-                    evtObj.sender.chartInterval = setInterval(function() {
+                    evtObj.sender.chartInterval = setInterval(function () {
                         (fuelVolume < 10) ? (fuelVolume = 80) : "";
                         var consVolume = (parseInt(snap.val()));
                         evtObj.sender.feedData && evtObj.sender.feedData("&value=" + consVolume);
@@ -86,35 +82,35 @@
                 },
                 //Using real time update event to update the annotation
                 //showing available volume of Diesel
-                "realTimeUpdateComplete": function(evt, arg) {
+                "realTimeUpdateComplete": function (evt, arg) {
                     var annotations = evt.sender.annotations,
                         dataVal = evt.sender.getData(),
                         colorVal = (dataVal >= 70) ? "#6caa03" : ((dataVal <= 35) ? "#e44b02" : "#f8bd1b");
                     //Updating value
                     annotations && annotations.update('rangeText', {
-                        "text": dataVal + " ltrs"
+                        "text":dataVal + " cm^3"
                     });
                     //Changing background color as per value
                     annotations && annotations.update('rangeBg', {
                         "fillcolor": colorVal
                     });
-        
+
                 },
-                "disposed": function(evt, arg) {
+                "disposed": function (evt, arg) {
                     clearInterval(evt.sender.chartInterval);
                 }
             }
         }
         );
-                    chartObj.render();
-                
+        chartObj.render();
+
     });
 
+
+
+
     dbRefListTemp.on('value', snap => {
-        const li = document.createElement('li');
-        li.innerText = "Teperature = " + snap.val();
-        ulList.appendChild(li);
-     
+
         var chartObj = new FusionCharts({
             type: 'thermometer',
             renderAt: 'chart-container2',
@@ -127,7 +123,7 @@
                     "subcaption": " Central cold store",
                     "lowerLimit": "-10",
                     "upperLimit": "40",
-        
+
                     "decimals": "1",
                     "numberSuffix": "°C",
                     "showhovereffect": "1",
@@ -141,7 +137,7 @@
                     "valueFontColor": "#000000",
                     "theme": "fusion",
                     "showValue": "1"
-                    
+
                 },
                 "value": "0",
                 //All annotations are grouped under this element
@@ -151,47 +147,36 @@
                         //Each group needs a unique ID
                         "id": "indicator",
                         "items": [
-                            //Showing Annotation
-                            {
-                                "id": "background",
-                                //Rectangle item
-                                "type": "rectangle",
-                                "alpha": "50",
-                                "fillColor": "#AABBCC",
-                                "x": "$gaugeEndX-40",
-                                "tox": "$gaugeEndX",
-                                "y": "$gaugeEndY+54",
-                                "toy": "$gaugeEndY+72"
-                            }
+                           
                         ]
                     }]
-        
+
                 },
             },
             "events": {
-                "rendered": function(evt, arg) {
-                    evt.sender.dataUpdate = setInterval(function() {
+                "rendered": function (evt, arg) {
+                    evt.sender.dataUpdate = setInterval(function () {
                         var value,
                             prevTemp = evt.sender.getData(),
                             mainTemp = (Math.random() * 10) * (-1),
                             diff = Math.abs(prevTemp - mainTemp);
-        
+
                         diff = diff > 1 ? (Math.random() * 1) : diff;
                         if (mainTemp > prevTemp) {
                             value = prevTemp + diff;
                         } else {
                             value = prevTemp - diff;
                         }
-        
+
                         evt.sender.feedData("&value=" + snap.val());
-        
+
                     }, 3000);
-                    evt.sender.updateAnnotation = function(evtObj, argObj) {
+                    evt.sender.updateAnnotation = function (evtObj, argObj) {
                         var code,
                             chartObj = evtObj.sender,
                             val = chartObj.getData(),
                             annotations = chartObj.annotations;
-        
+
                         if (val >= -4.5) {
                             code = "#00FF00";
                         } else if (val < -4.5 && val > -6) {
@@ -204,21 +189,23 @@
                         });
                     };
                 },
-                'renderComplete': function(evt, arg) {
+                'renderComplete': function (evt, arg) {
                     evt.sender.updateAnnotation(evt, arg);
                 },
-                'realtimeUpdateComplete': function(evt, arg) {
+                'realtimeUpdateComplete': function (evt, arg) {
                     evt.sender.updateAnnotation(evt, arg);
                 },
-                'disposed': function(evt, arg) {
+                'disposed': function (evt, arg) {
                     clearInterval(evt.sender.dataUpdate);
                 }
             }
         }
         );
-                    chartObj.render();
+        chartObj.render();
     });
-    
+
+
+
 
 }());
 
